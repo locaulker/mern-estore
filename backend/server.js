22c52265eecca0cb6f45e9ab28bda7d96e5f84c2
Lawrence O. Caulker <locaulker@gmail.com>
2020-11-07 04:24:15 -0500

    Installed and configured dotenv for environment variables

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,8 @@
 const express = require('express')
+const dotenv = require('dotenv')
 const products = require('./data/products')
+
+dotenv.config()
 
 const app = express()
 
@@ -16,4 +19,10 @@ app.get('/api/products/:id', (req, res) => {
   res.json(product)
 })
 
-app.listen(5000, console.log('Server running on port 5000'))
+const MODE = process.env.NODE_ENV
+const PORT = process.env.PORT || 5000
+
+app.listen(
+  5000,
+  console.log(`Server running in '${MODE}' mode on port ${PORT}`)
+)
