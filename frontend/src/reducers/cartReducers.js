cc33f966f8aacb1d3074d4a6f24db307f7319749
Lawrence O. Caulker <locaulker@gmail.com>
2020-11-15 02:37:43 -0500

    Remove Items from cart implemented

--- frontend/src/reducers/cartReducers.js
+++ frontend/src/reducers/cartReducers.js
@@ -1,10 +1,11 @@
-import { CART_ADD_ITEM } from '../constants/cartConstants'
+import { CART_ADD_ITEM, CART_REMOVE_ITEM } from '../constants/cartConstants'
 
 export const cartReducer = (state = { cartItems: [] }, action) => {
   switch (action.type) {
     case CART_ADD_ITEM:
       const item = action.payload
 
+      // const existItem = state.cartItems.find(x => x.product === item.product)
       const existItem = state.cartItems.find(x => x.product === item.product)
 
       if (existItem) {
@@ -20,6 +21,13 @@ export const cartReducer = (state = { cartItems: [] }, action) => {
           cartItems: [...state.cartItems, item],
         }
       }
+
+    case CART_REMOVE_ITEM:
+      return {
+        ...state,
+        cartItems: state.cartItems.filter(x => x.product !== action.payload),
+      }
+
     default:
       return state
   }
