e9ed9b59e20a8853266818dafa4ef36a7a3c3008
Lawrence <locaulker@gmail.com>
2020-11-19 06:16:58 -0500

    Show user in Navbar and added logout functionality

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -3,18 +3,19 @@ import {
   USER_LOGIN_FAIL,
   USER_LOGIN_REQUEST,
   USER_LOGIN_SUCCESS,
+  USER_LOGOUT
 } from '../constants/userConstants'
 
 export const login = (email, password) => async dispatch => {
   try {
     dispatch({
-      type: USER_LOGIN_REQUEST,
+      type: USER_LOGIN_REQUEST
     })
 
     const config = {
       headers: {
-        'Content-Type': 'application/json',
-      },
+        'Content-Type': 'application/json'
+      }
     }
 
     const { data } = await axios.post(
@@ -25,7 +26,7 @@ export const login = (email, password) => async dispatch => {
 
     dispatch({
       type: USER_LOGIN_SUCCESS,
-      payload: data,
+      payload: data
     })
 
     localStorage.setItem('userInfo', JSON.stringify(data))
@@ -35,7 +36,12 @@ export const login = (email, password) => async dispatch => {
       payload:
         error.response && error.response.data.message
           ? error.response.data.message
-          : error.message,
+          : error.message
     })
   }
+}
+
+export const logout = () => dispatch => {
+  localStorage.removeItem('userInfo')
+  dispatch({ type: USER_LOGOUT })
 }
